d6f5824c328c76bf620d05ae4cf9fcdc637d0428
Matt Elliott <melliott@ccistudios.com>
2012-06-05 09:19:17 -0400

    deploy and staging script set up

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,10 +1,10 @@
 # repository info
-set :branch, "development"
+set :branch, "master"
 
 # This may be the same as your `Web` server
-role :app, "ADDRESS"
+role :app, "ufh.ccistaging.com"
 
 # directories
-set :deploy_to, "/home/ACCOUNT/subdomains/DOMAIN"
+set :deploy_to, "/home/staging/subdomains/ufh"
 set :public, "#{deploy_to}/public_html"
 set :extensions, %w[public template]
